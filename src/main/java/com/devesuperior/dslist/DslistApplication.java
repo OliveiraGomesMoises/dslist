fdafe6bb661f21358cc2a2fa74ed9e33e1093493
OliveiraGomesMoises <mosesgdo@gmail.com>
2025-08-26 13:20:14 -0300

    properties, games, orm

--- src/main/java/com/devesuperior/dslist/DslistApplication.java
+++ src/main/java/com/devesuperior/dslist/DslistApplication.java
@@ -2,18 +2,10 @@ package com.devesuperior.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/")
 public class DslistApplication {
-	@GetMapping
-	public String getHome(){
-		return "IntensivaoOOOOOO";
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DslistApplication.class, args);
 	}
